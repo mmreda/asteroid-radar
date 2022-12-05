d0f7293cf760046dfd36ade9fde720d41b3359fe
Mohamed Reda <88160708+mmreda@users.noreply.github.com>
2022-12-05 19:38:57 +0200

    Update Constants.kt

--- app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,8 +4,5 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "YMLVviEstRTzxe3mHtnlJqg12GAMksmNprgKW7tk"
-
-
-
+    const val API_KEY = "DEMO_KEY" // please replace DEMO_KEY with your API key
 }
